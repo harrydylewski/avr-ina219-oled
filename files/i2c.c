ebf35469ecac36b7496b6911c4d26ea4b175a7ed
cyclonus101 <hdylewski@hotmail.com>
2018-03-12 21:41:36 -0400

    Update i2c.c

--- files/i2c.c
+++ files/i2c.c
@@ -17,15 +17,15 @@ void i2c_start(void)
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
     while (!(TWCR & (1<<TWINT)));
 }
-//send stop signal
+
 void i2c_stop(void)
 {
     TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 }
 
-void i2c_write(unsigned char u8data)
+void i2c_write(unsigned char data)
 {
-    TWDR = u8data;
+    TWDR = data;
     TWCR = (1<<TWINT)|(1<<TWEN);
     while (!(TWCR & (1<<TWINT)));
 }
@@ -36,7 +36,7 @@ unsigned char i2c_read_ack(void)
     while ((TWCR & (1<<TWINT)) == 0);
     return TWDR;
 }
-//read byte with NACK
+
 unsigned char i2c_read_nack(void)
 {
     TWCR = (1<<TWINT)|(1<<TWEN);
@@ -46,64 +46,6 @@ unsigned char i2c_read_nack(void)
 
 unsigned char i2c_getstatus(void)
 {
-    uint8_t status;
-    //mask status
-    status = TWSR & 0xF8;
+    unsigned char status = TWSR & 0xF8;
     return status;
 }
-
-//
-/*
-uint8_t EEWriteByte(uint16_t u16addr, uint8_t u8data)
-{
-    TWIStart();
-    if (TWIGetStatus() != 0x08)
-        return ERROR;
-    //select devise and send A2 A1 A0 address bits
-    TWIWrite((EEDEVADR)|(uint8_t)((u16addr & 0x0700)>>7));
-    if (TWIGetStatus() != 0x18)
-        return ERROR;  
-    //send the rest of address
-    TWIWrite((uint8_t)(u16addr));
-    if (TWIGetStatus() != 0x28)
-        return ERROR;
-    //write byte to eeprom
-    TWIWrite(u8data);
-    if (TWIGetStatus() != 0x28)
-        return ERROR;
-    TWIStop();
-    return SUCCESS;
-}
-
-//
-
-uint8_t EEReadByte(uint16_t u16addr, uint8_t *u8data)
-{
-    //uint8_t databyte;
-    TWIStart();
-    if (TWIGetStatus() != 0x08)
-        return ERROR;
-    //select devise and send A2 A1 A0 address bits
-    TWIWrite((EEDEVADR)|((uint8_t)((u16addr & 0x0700)>>7)));
-    if (TWIGetStatus() != 0x18)
-        return ERROR;
-    //send the rest of address
-    TWIWrite((uint8_t)(u16addr));
-    if (TWIGetStatus() != 0x28)
-        return ERROR;
-    //send start
-    TWIStart();
-    if (TWIGetStatus() != 0x10)
-        return ERROR;
-    //select devise and send read bit
-    TWIWrite((EEDEVADR)|((uint8_t)((u16addr & 0x0700)>>7))|1);
-    if (TWIGetStatus() != 0x40)
-        return ERROR;
-    *u8data = TWIReadNACK();
-    if (TWIGetStatus() != 0x58)
-        return ERROR;
-    TWIStop();
-    return SUCCESS;
-}
-*/
-
